4ee29bef923f421846fb71bea21c04e93fa730e4
Sales <crowzcoleslaw@gmail.com>
2020-10-24 00:39:48 -0300

    delete de transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+
+    const deleteTransaction = await transactionRepository.delete(id);
+
+    if (deleteTransaction.affected === 0) {
+      throw new AppError('Transaction does not exist!');
+    }
   }
 }
 
